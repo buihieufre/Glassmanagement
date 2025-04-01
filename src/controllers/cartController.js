d1aaa9cbf50c39f9a4a5583e7f43cbee813f64b0
Bui Hieu <buidinhhieu9b@gmail.com>
2025-04-01 15:32:53 +0700

    Update add to cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -6,16 +6,21 @@ exports.addToCart = async (req, res) => {
         const { userId, productId, quantity } = req.body;
 
         const product = await Product.findByPk(productId);
-
+        console.log(quantity, product.quantity);
+        if (quantity > product.quantity) {
+            return res.status(400).json({ message: "Không đủ số lượng" });
+        }
         const cart = await Cart.findOne({ where: { userId } });
 
         if (cart) {
             const cartId = cart.cartId; // Ensure cartId is a string if required by the database
-            const prod = await CartItem.findOne({where: {
-                productId: productId,
-                cartId: cartId
-            }})
-            if(prod) {
+            const prod = await CartItem.findOne({
+                where: {
+                    productId: productId,
+                    cartId: cartId,
+                },
+            });
+            if (prod) {
                 const newQtt = prod.quantity + quantity;
                 await CartItem.update(
                     { quantity: newQtt },
@@ -26,7 +31,7 @@ exports.addToCart = async (req, res) => {
                         },
                     }
                 );
-            }else {
+            } else {
                 await CartItem.create({
                     cartId: cart.cartId,
                     productId,
@@ -36,7 +41,7 @@ exports.addToCart = async (req, res) => {
             }
         } else {
             const newCart = await Cart.create({ userId });
-            newCart.cartId = newCart.cartId; // Ensure cartId is a string if required by the database
+            newCart.cartId = newCart.cartId;
             await CartItem.create({
                 cartId: newCart.cartId,
                 productId,
@@ -44,21 +49,22 @@ exports.addToCart = async (req, res) => {
                 price: product.price,
             });
         }
-        const curCart = await Cart.findOne({where: {
-            userId
-        }})
+        const curCart = await Cart.findOne({
+            where: {
+                userId,
+            },
+        });
         const productIds = await CartItem.findAll({
             where: { cartId: curCart.cartId },
-            attributes: ['productId']
-        }).then(items => items.map(item => item.productId));
+            attributes: ["productId"],
+        }).then((items) => items.map((item) => item.productId));
 
         const productsInCart = await Product.findAll({
             where: {
-            id: productIds
-            }
+                id: productIds,
+            },
         });
         res.status(200).json({ message: "Thêm vào giỏ hàng thành công", data: productsInCart });
-
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: "Lỗi khi thêm vào giỏ hàng", error });
@@ -121,3 +127,38 @@ exports.removeFromCart = async (req, res) => {
         return res.status(500).json({ message: "Lỗi khi xóa sản phẩm khỏi giỏ hàng", error });
     }
 };
+
+exports.updateQuantity = async (req, res) => {
+    try {
+        const { userId, productId, quantity } = req.body;
+        const cart = await Cart.findOne({
+            where: {
+                userId,
+            },
+        });
+        const cartItem = await CartItem.findOne({
+            where: {
+                cartId: cart.cartId,
+                productId: productId,
+            },
+        });
+        if (quantity >= 1) {
+            const productInstance = await Product.findOne({ where: { id: productId } });
+
+            if (quantity > productInstance.quantity) {
+                return res
+                    .status(200)
+                    .json({ message: "Không thể thêm nữa vì đã vượt quá số lượng hàng trong kho" });
+            } else {
+                await cartItem.update({ quantity: quantity });
+                return res.status(200);
+            }
+        } else {
+            await cartItem.destroy();
+            return res.status(200).json({ message: "Xóa sản phẩm khỏi giỏ hàng thành công" });
+        }
+    } catch (error) {
+        console.error("Lỗi khi update quantity");
+        return res.status(500).json({ message: "Server error" });
+    }
+}; 
--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -4,4 +4,5 @@ const cartController = require('../controllers/cartController')
 router.post("/add", cartController.addToCart)
 router.get("/get/:userId", cartController.getCartItems);
 router.post("/delete", cartController.removeFromCart);
+router.post("/update", cartController.updateQuantity);
 module.exports = router
